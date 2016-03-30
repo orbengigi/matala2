5d7417c86af47a0989103182c6fc5a537a8146f7
Ido <idonava@gmail.com>
2016-03-30 15:41:14 +0300

    ido

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+print("Opened database successfully")
+
+conn.execute('''CREATE TABLE COMPANY3
+       (ID INT PRIMARY KEY     NOT NULL,
+       NAME           TEXT    NOT NULL,
+       AGE            INT     NOT NULL,
+       ADDRESS        CHAR(50),
+       SALARY         REAL);''')
+print("Table created successfully")
+
+conn.close()
+
+#!/usr/bin/python
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+print("Opened database successfully")
+
+conn.execute("INSERT INTO COMPANY3 (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+
+conn.commit()
+print("Records created successfully")
+conn.close()
