41070e65d6f7e5c2cf1753995f280d7bab289b49
or <or.bngigi@gmail.com>
2016-03-30 17:11:54 +0300

    up2

--- M1.py
+++ M1.py
@@ -14,20 +14,19 @@ def read_dir(str):
                read_file(file)
 
 
-def read_file(str):
-    with open (str,'r')as f:
+def read_file(str1):
+    with open (str1,'r')as f:
+        c.execute('drop table if exists nmea' + str(i))
+        c.execute('''CREATE TABLE nmea''' + str(i) + '''
+                (time text, latitude text,north text, longtitude text,
+                east text,quality text, nos text, hdop text, altitude text,
+                hog text)''')
         for x in f:
             if "GPGGA" in x:
                 create_DB(x)
 
 def create_DB(str1):
     list1=str1.split(",")
-    c.execute('drop table if exists nmea'+str(i))
-    c.execute('''CREATE TABLE nmea'''+str(i)+'''
-        (time text, latitude text,north text, longtitude text,
-        east text,quality text, nos text, hdop text, altitude text,
-        hog text)''')
-
     # Insert a row of data
     c.execute("INSERT INTO nmea"+str(i)+" VALUES (?,?,?,?,?,?,?,?,?,?)",(list1[1], list1[2], list1[3], list1[4],list1[5], list1[6],list1[7], list1[8], list1[9], list1[10]))
     conn.commit()
--- test.py
+++ test.py
@@ -2,30 +2,34 @@
 
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-print("Opened database successfully")
 
-conn.execute('''CREATE TABLE COMPANY3
-       (ID INT PRIMARY KEY     NOT NULL,
-       NAME           TEXT    NOT NULL,
-       AGE            INT     NOT NULL,
-       ADDRESS        CHAR(50),
-       SALARY         REAL);''')
-print("Table created successfully")
-dd
-conn.close()
+def create_db():
+    conn = sqlite3.connect('test.db')
+    c=conn.cursor()
+    c.execute(""""drop table if exists nmea""")
 
-#!/usr/bin/python
+    conn.commit()
+    c.execute('drop table if exists nmea')
+    conn.execute('CREATE TABLE nmea'
+                 '        (ID INT PRIMARY KEY     NOT NULL,\n'
+                 '        time TEXT, latitude STRING,north STRING,\n'
+                 '        longtitude STRING,east STRING,quality STRING, nos STRING,\n'
+                 '        hdop STRING, altitude STRING, hog STRING)')
+    print("Table created successfully")
 
-import sqlite3
-
-conn = sqlite3.connect('test.db')
-print("Opened database successfully")
-
-conn.execute("INSERT INTO COMPANY3 (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+    conn.close()
 
 
-conn.commit()
-print("Records created successfully")
-conn.close()
+def add_to_db(str):
+    list1=str.split(',')
+    print (list1[1][1])
+    conn = sqlite3.connect('test.db')
+    print("Opened database successfully")
+    conn.execute("INSERT INTO nmea VALUES (2, '2',?,'4','5','6','7','8','91','10','11')",[list1[1]])
+
+    conn.commit()
+    print("Records created successfully")
+    conn.close()
+
+create_db()
+add_to_db("or,roni,ido")
